c21e4f5af48c3196c3a301db21ec718d2d7ec888
Pierre Héroux <Pierre.Heroux@gmail.com>
2025-12-23 00:35:35 +0100

    Peaufinage de la page de blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
   <div class="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
     <div class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
       <div>
-        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Blog de Cyclopolis</h2>
+        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Blog</h2>
         <p class="mt-3 text-xl text-gray-500 sm:mt-4">
           Quelques articles et autres réflexions sur la plateforme de {{ getAssoName() }}.
         </p>
@@ -45,7 +45,7 @@
           <div class="mt-4">
             <p class="text-xl font-semibold text-gray-900">Historique des changements</p>
             <p class="mt-3 text-base text-gray-500">
-              Retrouver l'historique des mises à jour de la plateforme Cyclopolis.
+              Retrouver l'historique des mises à jour de la plateforme.
             </p>
           </div>
         </NuxtLink>
@@ -62,8 +62,7 @@
           <div class="mt-4">
             <p class="text-xl font-semibold text-gray-900">Sites partenaires</p>
             <p class="mt-3 text-base text-gray-500">
-              Découvrez d'autres observatoires à travers le pays, dont certains sont basés sur notre travail en Open
-              Source.
+              Découvrez d'autres observatoires à travers le pays, dont certains sont, comme le nôtre, basés sur la plateforme Open Source Cyclopolis.
             </p>
           </div>
         </NuxtLink>
